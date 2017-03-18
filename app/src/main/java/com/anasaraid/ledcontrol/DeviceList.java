7ba2968fee2ec812ad05f958f3915df2bf18267b
asdf1899 <araid.anas99@gmail.com>
2017-03-18 16:47:47 +0100

    check if bluetooth adapter is activated and add pairedDevicesList method

--- app/src/main/java/com/anasaraid/ledcontrol/DeviceList.java
+++ app/src/main/java/com/anasaraid/ledcontrol/DeviceList.java
@@ -1,5 +1,6 @@
 package com.anasaraid.ledcontrol;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -21,10 +22,19 @@ import android.content.Intent;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 
 public class DeviceList extends AppCompatActivity {
     private BluetoothAdapter myBluetooth = null;
-    private Set pairedDevice;
+    private Set pairedDevices;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,9 +44,44 @@ public class DeviceList extends AppCompatActivity {
         setSupportActionBar(toolbar);
         Button btnPaired;
         ListView deviceList;
-        btnPaired = (Button)findViewById(R.id.btnPairedDevies);
-        deviceList = (ListView)findViewById(R.id.lstPairedDevices);
+        btnPaired = (Button) findViewById(R.id.btnPairedDevies);
+        deviceList = (ListView) findViewById(R.id.lstPairedDevices);
+        myBluetooth = BluetoothAdapter.getDefaultAdapter();
+        if (myBluetooth == null) {
+            Toast.makeText(getApplicationContext(), "Bluetooth device not available", Toast.LENGTH_LONG).show();
+            finish();
+        } else {
+            if (myBluetooth.isEnabled()) {
 
+            } else {
+                Intent turnBTOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(turnBTOn, 1);
+            }
+            btnPaired.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v){
+                    pairedDevicesList();
+                }
+            });
+        }
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+    private void pairedDevicesList(){
+        BluetoothDevice bt;
+        pairedDevices = myBluetooth.getBondedDevices();
+        ArrayList list = new ArrayList();
+        if (pairedDevices.size() > 0){
+            for (bt : pairedDevices)
+            {
+                list.add(bt.getName() + "\n" + bt.getAddress());
+            }
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(), "No paired devices found", Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
@@ -59,5 +104,45 @@ public class DeviceList extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "DeviceList Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.anasaraid.ledcontrol/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "DeviceList Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.anasaraid.ledcontrol/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
